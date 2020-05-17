0cb0a6a9ddf1984444a9d3c895cfd9c73e637e09
vinodkotha <vennivinod@gmail.com>
2020-05-17 16:50:31 -0400

    plugged Router to the App and used withRouter HOD function to access history match location in child component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="body">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherValueProps }) => (
+          <MenuItem key={id} {...otherValueProps} />
         ))}
       </div>
     );
